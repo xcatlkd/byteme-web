b37a21b54d6bb5a8d1defbff5dec78538c6acd44
Mr Fox-L <feralseattle#gmail.com>
2017-09-02 11:23:51 -0400

    Redux setup on src/App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import './App.scss';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// redux setup ##########################################
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(reduxThunk));
+
 import Home from "pages/Home";
 import Navigation from "./components/Navigation";
 import SignUp from "pages/SignUp";
@@ -12,18 +21,20 @@ import PageError from "pages/404";
 class App extends React.Component {
 	render() {
 		return (
-    	<BrowserRouter>
-      	<div className="navbar">
-					<Navigation/>
-        	<Switch>
-          	<Route exact path= "/" component={Home}/>
-          	<Route exact path="/Signup" component={SignUp}/>
-          	<Route exact path="/Login" component={Login}/>
-          	<Route exact path="/UserAdmin" component={UserAdmin}/>
-						<Route exact path="/*" component={PageError}/>
-        	</Switch>
-        </div>
-      	</BrowserRouter>
+      <Provider store={store}>
+      	<BrowserRouter>
+        	<div className="navbar">
+  					<Navigation/>
+          	<Switch>
+            	<Route exact path= "/" component={Home}/>
+            	<Route exact path="/Signup" component={SignUp}/>
+            	<Route exact path="/Login" component={Login}/>
+            	<Route exact path="/UserAdmin" component={UserAdmin}/>
+  						<Route exact path="/*" component={PageError}/>
+          	</Switch>
+          </div>
+        	</BrowserRouter>
+        </Provider>
     	);
   	}
 }
