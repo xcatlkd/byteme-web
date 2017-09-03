b01923bf0548ee946151f9546c4066aab9d4ccac
Mr Fox-L <feralseattle#gmail.com>
2017-09-03 10:04:54 -0400

    Working on api routes.

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,10 +2,19 @@ import express from "express";
 import BodyParser from "body-parser";
 
 // import models here ##################################
-
+import Post from "server/models/post";
 
 
 const router = express.Router();
 router.use(BodyParser.json());
 
 // define routes here ###################################
+
+router.get("/posts", (req, res) {
+	Post.findAll({
+		where: id === req.session.restaurantId,
+	})
+	.then(() => {
+		
+	})
+});
