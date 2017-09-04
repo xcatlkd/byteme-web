3bd28298aa43e83b3569da30f5cd16a80f10a568
MrFox-L <feralseattle@gmail.com>
2017-09-04 15:38:37 -0400

    Error checking. No results.

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,30 @@ import Navigation from "./components/Navigation";
 import SignUp from "pages/SignUp";
 import Login from "pages/Login";
 import UserAdmin from "pages/UserAdmin";
+import Upload from "pages/Upload";
 import PageError from "pages/404";
 
 
 class App extends React.Component {
-	render() {
-		return (
+  render() {
+    return (
       <Provider store={store}>
-      	<BrowserRouter>
-        	<div className="navbar">
-  					<Navigation/>
-          	<Switch>
-            	<Route exact path= "/" component={Home}/>
-            	<Route exact path="/Signup" component={SignUp}/>
-            	<Route exact path="/Login" component={Login}/>
-            	<Route exact path="/UserAdmin" component={UserAdmin}/>
-  						<Route exact path="/*" component={PageError}/>
-          	</Switch>
+        <BrowserRouter>
+          <div className="navbar">
+            <Navigation/>
+            <Switch>
+              <Route exact path= "/" component={Home}/>
+              <Route exact path="/Signup" component={SignUp}/>
+              <Route exact path="/Login" component={Login}/>
+              <Route exact path="/UserAdmin" component={UserAdmin}/>
+              <Route exact path="/Upload" component={Upload}/>
+              <Route exact path="/*" component={PageError}/>
+            </Switch>
           </div>
-        	</BrowserRouter>
+          </BrowserRouter>
         </Provider>
-    	);
-  	}
+      );
+    }
 }
 
 export default App;
---
+++ src/pages/Upload.jsx
@@ -0,0 +1,32 @@
+import "./Upload.scss";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import PropTypes from "prop-types";
+
+class Upload extends Component {
+	render() {
+		return (
+			<div className="upload-container">
+				<h1>Upload Your Photos Here</h1>
+				<form onSubmit={this._handleSubmit} method="post">
+					<label className="file" name="file" onChange={this._handleChange} required />
+				</form>
+			</div>
+		);
+	}
+}
+
+Upload.propTypes = {
+
+};
+
+function mapStateToProps(state, props) {
+	return {
+
+	};
+
+}
+
+
+export default connect(mapStateToProps, {  })(Upload);
