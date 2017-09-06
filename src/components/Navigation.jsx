4ba7007fb51cbbe7cd2810ae40c81d356fbb5d81
Jane <janeparkjp@gmail.com>
2017-09-06 16:22:28 -0400

    basic navbar layout designed

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,10 @@
 import "./Navigation.scss";
 import React, { Component } from "react";
 import { NavLink, Link } from "react-router-dom";
+import { Button, Menu } from "semantic-ui-react";
 
 class Navigation extends Component {
+
 	render() {
 		const links = [{
 			to: "/signup",
@@ -13,27 +15,45 @@ class Navigation extends Component {
 		}];
 
 		return (
-			<nav className="Nav">
-				<Link to="/" className="Link-Home">
-					<div className="logo">
-						<img src="/src/assets/images/restauranticon.png" />
-					Byte Me
-					</div>
-				</Link>
-				{links.map((link) => {
-					return (
-						<NavLink
-							key={link.to}
-							to={link.to}
-							className="Nav-link"
-							activeClassName="is-active"
-							exact
-						>
-							{link.text}
+			<div className="Nav">
+				<Menu>
+					<Menu.Menu position = "left">
+						{/* <Menu.Item position = "left"> */}
+							<Link to="/" className="Link-Home">
+								<div className="logo">
+									<img src="/src/assets/images/restauranticon.png" />
+										Byte Me
+								</div>
+							</Link>
+						{/* </Menu.Item> */}
+					</Menu.Menu>
+						<Menu.Menu position = "right">
+							<div className="nav-buttons">
+							<Link to="/signup">
+								<Button>Sign Up</Button>
+							</Link>
+							<Link to="/login">
+								<Button>Login</Button>
+							</Link>
+						</div>
+						{/* {links.map((link) => {
+							return (
+								<Button key={link}>
+								<NavLink
+									key={link.to}
+									to={link.to}
+									className="Nav-link"
+									activeClassName="is-active"
+									exact
+									>
+									{link.text}
 						</NavLink>
-					);
-				})}
-			</nav>
+					</Button> */}
+					{/* );
+				})} */}
+			</Menu.Menu>
+	</Menu>
+</div>
 		);
 	}
 }
