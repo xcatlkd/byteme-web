3f757112fa394b93624dfe125cd49da3844bf0df
Jane <janeparkjp@gmail.com>
2017-09-07 10:07:05 -0400

    trying to render images on useradmin

--- src/pages/UserAdmin.jsx
+++ src/pages/UserAdmin.jsx
@@ -2,6 +2,10 @@ import "./UserAdmin.scss";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+<<<<<<< Updated upstream
+=======
+import Test from "json/test.json";
+>>>>>>> Stashed changes
 import PropTypes from "prop-types";
 
 class UserAdmin extends Component {
