e264b2060020b2cf8530a6dafb4607a7dacc3aa7
MrFox-L <feralseattle@gmail.com>
2017-09-08 13:12:31 -0400

    Post model built. New packages installed, run npm install.

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -1,9 +1,9 @@
 import sql from '../util/sql';
 const Sequelize = require('sequelize');
 const bcrypt = require('bcrypt');
+import Jimp from 'jimp';
 
 const path = require('path');
-import imgurUpload from "../util/imgurUpload";
 
 // import table dependencies
 
@@ -44,37 +44,45 @@ const Restaurant = sql.define('restaurant', {
 });
 
 // relationships
-//Restaurant.hasMany(Post);
+Restaurant.hasMany(Post);
 /* Restaurant.hasOne(Address, { through: userAddress });
 Restaurant.hasMany(Phone, { through: userPhone }); */
 
 // model extensions
-function handleUpload(product) {
-	if (!product.originalImages) {
-		return;
-	}
+Restaurant.prototype.upload = function(file, body) {
+	let image;
+	return this.createPost({
+			id: file.filename,
+			size: file.size,
+			originalName: file.originalname,
+			mimeType: file.mimetype,
+			title: body.title,
+			description: body.description,
+			price: body.price,		  
+		})
+		.then(function(data) {
+			image = data;
+			const ext = path.extname(file.originalname);
+			const dest = "assets/photos/" + file.filename + ext;
+			return	fs.copy(file.path, dest)
+		})
+		.then(function() {
+			// If I'm an image
+			if (file.mimetype.includes("image/")) {
+				return Jimp.read(file.path)
+			.then(function(img) {
+					img.quality(80);
+					img.resize(Jimp.AUTO, 400);
+					// img.create(file.filename);
+					return	img.write("assets/files/" + file.filename + ".jpg")
+			});
+			}
+			})
+		.then(function() {
+					return image;
+		});
+};
 
-	const allOldImages = product.images || [];
-
-	const promises = [];
-
-	product.originalImages.forEach((image, idx) => {
-		const oldImages = allOldImages[idx] || {};
-		if (image && image !== oldImages.original) {
-			promises.push(uploadToImgur(image).then((images) => {
-				product.images = [...product.images];
-				product.images[idx] = images;
-			}));
-		}
-	});
-
-	return Promise.all(promises).then(() => {
-		return product;
-	}).catch((err) => {
-		console.warn("Encountered error while uploading product image. Saving without images.");
-		console.warn(err.message);
-	});
-}
 
 
 //additional user functionality
