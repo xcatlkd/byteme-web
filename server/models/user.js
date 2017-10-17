2cd771ba6008786168aa663ccbbf7406fee4125d
Mr Fox-L <feralseattle#gmail.com>
2017-10-17 14:00:12 -0400

    User model and api/user routes configured.

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,13 @@
 
-const sql = require('../util/sql');
+import sql from '../util/sql';
 const Sequelize = require('sequelize');
 const bcrypt = require('bcrypt');
 
 const path = require('path');
-const Jimp = require('jimp');
-const Files = require('./file');
 // import table dependencies
 
 const Likes = require('./like');
-const Comments = require('./comment');
 const fs = require("fs-extra");
-
-
 
 const User = sql.define('user', {
 	id: {
@@ -38,40 +33,6 @@ const User = sql.define('user', {
 		beforeUpdate: hashUserPassword,
 	},
 });
-
-
-// User.prototype.upload = function(file, body) {
-// 	let image;
-// 	return this.createFile({
-// 			id: file.filename,
-// 			size: file.size,
-// 			originalName: file.originalname,
-// 			mimeType: file.mimetype,
-// 			description: body.description,
-		  
-// 		})
-// 		.then(function(data) {
-// 			image = data;
-// 			const ext = path.extname(file.originalname);
-// 			const dest = "assets/photos/" + file.filename + ext;
-// 			return	fs.copy(file.path, dest)
-// 		})
-// 		.then(function() {
-// 			// If I'm an image
-// 			if (file.mimetype.includes("image/")) {
-// 				return Jimp.read(file.path)
-// 			.then(function(img) {
-// 					img.quality(80);
-// 					img.resize(Jimp.AUTO, 300);
-// 					// img.create(file.filename);
-// 					return	img.write("assets/files/" + file.filename + ".jpg")
-// 			});
-// 			}
-// 			})
-// 		.then(function() {
-// 					return image;
-// 		});
-// };
 
 
 //additional user functionality
